37f714aa9385995b133401787984a8b2afbfbdbe
Jörn Schimmelpfeng <joern.schimmelpfeng@daimler.com>
2021-02-25 09:28:45 +0100

    Changed Logging config to properties Updated Swagger

--- src/main/kotlin/de/dhbw/askme/Application.kt
+++ src/main/kotlin/de/dhbw/askme/Application.kt
@@ -6,11 +6,11 @@ import io.swagger.v3.oas.annotations.info.*
 
 @OpenAPIDefinition(
     info = Info(
-        title = "Hello World",
-        version = "0.0",
-        description = "My API",
-        license = License(name = "Apache 2.0", url = "https://foo.bar"),
-        contact = Contact(url = "https://www.dhbw.de", name = "Joern", email = "joern@something")
+        title = "AskMe",
+        version = "0.1",
+        description = "AskMe API documentation for students sample project",
+        license = License(name = "Apache 2.0", url = "https://github.com/JoernSchimmelpfeng/askme/blob/master/LICENSE"),
+        contact = Contact(url = "https://github.com/JoernSchimmelpfeng", name = "Joern Schimmelpfeng", email = "")
     )
 )
 object Application {
